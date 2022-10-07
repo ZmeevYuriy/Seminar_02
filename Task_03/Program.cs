b3adfdb23f9b7aa9148fccd32342cc1df9edce84
Yuriy_Zmeev <yuriy.zmeev@mail.ru>
2022-10-07 11:50:12 +0600

    add to Task_03

---
+++ Task_03/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача № 15
+
+Console.Write("Введи цифру дня нидели: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+void DayWeek (int a) 
+{
+    if (a == 6 || a == 7) 
+{
+    Console.WriteLine("День нидели выходной: да");
+}
+  else if (a < 1 || a > 7) 
+{
+    Console.WriteLine("Такого дня нидели не существует");
+}
+  else Console.WriteLine("День нидели не выходной: нет");
+}
+
+DayWeek(a);
