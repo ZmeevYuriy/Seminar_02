782244a9eb7e8c6b12995ad0a06c4839257bb6d0
Yuriy_Zmeev <yuriy.zmeev@mail.ru>
2022-10-07 11:48:03 +0600

    add to Task_02

---
+++ Task_02/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача № 13
+
+Console.WriteLine("Ведите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(a);
+
+if (stringNumber.Length > 2)
+{
+    Console.WriteLine("Третья цифра числа: " + stringNumber[2]);
+}
+
+else
+{
+    Console.WriteLine("Третья цифра числа отсутствует");
+}
