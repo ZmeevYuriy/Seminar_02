5379d8b25e171c491a0a126a00a13636e8a118cc
Yuriy_Zmeev <yuriy.zmeev@mail.ru>
2022-10-07 11:45:33 +0600

    add to Task_01

---
+++ Task_01/Program.cs
@@ -0,0 +1,7 @@
+﻿//Задача №10
+
+Console.WriteLine("Введите трехзначное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+string stringA = Convert.ToString(a);
+
+Console.WriteLine("Второй цифрой числа является: "+ stringA[1]);
